4260ad2b8bfa30bd67eba553c4f2b73983620ac2
Alejandro Salas <asalas.wd@outlook.com>
2022-11-15 19:27:17 -0500

    Initial commit

---
+++ src/com/java21days/FlowFrame.java
@@ -0,0 +1,12 @@
+package com.java21days;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowFrame extends JFrame {
+    Dimension dim = new Dimension(200, 50);
+
+    FlowFrame(String title){
+        super(title);
+    }
+}
