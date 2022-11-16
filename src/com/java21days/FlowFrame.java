ae06ab1fddb29bf6110af6d890b26a083738dce4
Alejandro Salas <asalas.wd@outlook.com>
2022-11-15 19:46:08 -0500

    Create FlowFrame instance in main method

--- src/com/java21days/FlowFrame.java
+++ src/com/java21days/FlowFrame.java
@@ -4,9 +4,24 @@ import javax.swing.*;
 import java.awt.*;
 
 public class FlowFrame extends JFrame {
-    Dimension dim = new Dimension(200, 50);
+    Dimension dim = new Dimension(400, 150);
 
-    FlowFrame(String title){
+    FlowFrame(String title, String[] buttons){
         super(title);
+        FlowLayout flow = new FlowLayout();
+        setLayout(flow);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(dim);
+        setLocationRelativeTo(null);
+
+
+
+        setVisible(true);
+    }
+
+    JButton[] createButtons(String[] buttons){
+        JButton[] jButtons = new JButton[buttons.length];
+
+        return jButtons;
     }
 }
--- src/com/java21days/Main.java
+++ src/com/java21days/Main.java
@@ -1,7 +1,10 @@
 package com.java21days;
 
+
 public class Main {
     public static void main(String[] args) {
+        String[] teams = { "Argentina", "Brazil", "France", "Germany", "USA", "Croatia", "Portugal" };
+        FlowFrame flowFrame = new FlowFrame("World Cup candidates", teams);
 
     }
 }
